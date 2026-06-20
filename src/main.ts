7a1f5b862682a8e445c2a88a87c324de35826e4f
code043 <procoder043@gmail.com>
2026-06-20 18:40:47 -0300

    refactor: cors origin

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://shop-xi-sage.vercel.app/',
     methods: 'GET,POST,PATCH,DELETE',
     credentials: true,
   });
